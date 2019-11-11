3e856f6d13e8fa22081477813483222abd25aea9
JP Cimalando <jp-dev@inbox.ru>
2019-11-11 16:56:38 +0100

    build with UI for macOS

--- .travis/script-macos.sh
+++ .travis/script-macos.sh
@@ -14,16 +14,15 @@ set -x
 # setup
 
 export PKG_CONFIG="pkg-config --static"
-export PKG_CONFIG_PATH="/opt/local/lib/pkgconfig"
+export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig"
 
-export PATH=/opt/local/bin:"$PATH"
+export PATH=/usr/local/bin:"$PATH"
 
-export CFLAGS="-I/opt/local/include"
-export CXXFLAGS="-I/opt/local/include"
-export LDFLAGS="-L/opt/local/lib"
+export CFLAGS="-I/usr/local/include"
+export CXXFLAGS="-I/usr/local/include"
+export LDFLAGS="-L/usr/local/lib"
 
 # ---------------------------------------------------------------------------------------------------------------------
 # build the plugin
 
-# TODO setup cairo UI build...
-MACOS=true BUILD_UI=false make $MAKE_ARGS
+MACOS=true make $MAKE_ARGS
